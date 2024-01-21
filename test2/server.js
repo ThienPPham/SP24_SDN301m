1b17f0e0e26e9fe6b285fa64a206045a9e32565f
ThienPPham <90206572+ThienPPham@users.noreply.github.com>
2024-01-21 13:57:50 +0700

    Test 2

---
+++ test2/server.js
@@ -0,0 +1,41 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'test2'
+const dboper = require('./operation');
+
+MongoClient.connect(url).then((client) =>{
+    console.log('Connected correctly to server');
+    const db = client.db(dbname);
+
+    dboper.insertDocument(db, {name:"Weekend Grand Buffet", image:"images/buffet.png", label:"New", price:"19.99", description:"Featuring...", featured: "false"}, "promotions")
+    .then((result) =>{
+        console.log("Insert Document:\n", result.ops);
+
+        return dboper.findDocuments(db, "promotions");
+    })
+    .then((docs) =>{
+        console.log("Found documents:\n", docs);
+
+        return dboper.updateDocument(db, {name:"Weekend Grand Buffet"}, {description: "Updated Featured"}, "promotions");
+    })
+    .then((result) =>{
+        console.log("Updated Document:\n", result.result);
+
+        return dboper.findDocuments(db, "promotions");
+    })
+    .then((docs) =>{
+        console.log("Found document updated:\n ", docs);
+
+        return db.dropCollection("promotions");
+    })
+    .then((result) =>{
+        console.log("Dropped Collection: ", result);
+
+        return client.close();
+    })
+    .catch((err) => console.log(err));
+})
+
+.catch((err) => console.log(err));
