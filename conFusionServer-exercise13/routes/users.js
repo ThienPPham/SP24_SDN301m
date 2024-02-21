36126d5799edcf88390d95dec3d706d1e10d5efa
ThienPPham <90206572+ThienPPham@users.noreply.github.com>
2024-02-21 20:56:04 +0700

    Passport

--- conFusionServer-exercise13/routes/users.js
+++ conFusionServer-exercise13/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 const bodyParser = require('body-parser');
 var User = require('../models/user');
@@ -11,7 +12,8 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next) => {
+//Exercise 17
+/*router.post('/signup', (req, res, next) => {
   User.findOne({ username: req.body.username })
     .then((user) => {
       if (user != null) {
@@ -71,8 +73,34 @@ router.post('/login', (req, res, next) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('You are already authenticated!');
   }
+});*/
+
+//Exercise 18
+router.post('/signup', (req, res, next) => {
+  User.register(new User({ username: req.body.username }),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+      }
+      else {
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ success: true, status: 'Registration Successful!' });
+        });
+      }
+    });
 });
 
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ success: true, status: 'You are successfully logged in!' });
+});
+
+//Exercise 17
 router.post('/logout', (req, res) => {
   if (req.session) {
     req.session.destroy();
